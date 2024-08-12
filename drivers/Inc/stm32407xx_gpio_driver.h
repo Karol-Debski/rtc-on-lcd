9aa50b56a19478f576c3cba4f81a4c1a32c761cb
Karol <karol3129@gmail.com>
2024-08-12 12:12:57 +0200

    Added header file and source file for gpio driver, added first implementations of functions

--- drivers/Inc/stm32407xx_gpio_driver.h
+++ drivers/Inc/stm32407xx_gpio_driver.h
@@ -10,4 +10,137 @@
 
 #include "stm32f407xx.h"
 
+typedef struct
+{
+	uint8_t GPIO_PinNumber;					// EGPIO_PinNumber
+	uint8_t GPIO_PinMode;					// EGPIO_Mode
+	uint8_t GPIO_PinOutputSpeed;			// EGPIO_OutputSpeed
+	uint8_t GPIO_PinPullUpPullDownMode;		// EGPIO_PullUpPullDownMode
+	uint8_t GPIO_PinOutputType;				// EGPIO_OutputType
+	uint8_t GPIO_PinAltFunMode;				//
+} GPIO_PinConfig_t;
+
+typedef struct
+{
+	GPIO_RegDef_t* pGPIOx;
+	GPIO_PinConfig_t GPIO_PinConfig;
+} GPIO_Handle_t;
+
+typedef enum EGPIO_ClockControlState
+{
+	GPIO_CLOCK_DISABLE = 0,
+	GPIO_CLOCK_ENABLE = 1,
+} EGPIO_ClockControlState;
+
+typedef enum EGPIO_IRQControlState
+{
+	GPIO_IRQ_DISABLE = 0,
+	GPIO_IRQ_ENABLE = 1,
+} GPIO_IRQControlState;
+
+
+typedef enum EGPIO_PinState
+{
+	GPIO_LOW = 0,
+	GPIO_HIGH = 1,
+} EGPIO_PinState;
+
+typedef enum EGPIO_Mode
+{
+	GPIO_MODE_INPUT = 0x0,
+	GPIO_MODE_OUTPUT = 0x1,
+	GPIO_MODE_ALT_FN = 0x2,
+	GPIO_MODE_ANALOG = 0x3,
+	GPIO_MODE_IT_FT = 4,
+	GPIO_MODE_IT_RT = 5,
+	GPIO_MODE_IT_RFT = 6,
+} EGPIO_Mode;
+
+typedef enum EGPIO_OutputType
+{
+	GPIO_OUTPUT_TYPE_PP = 0x0,
+	GPIO_OUTPUT_TYPE_OP = 0x1,
+} EGPIO_OutputType;
+
+typedef enum EGPIO_OutputSpeed
+{
+	GPIO_OUTPUT_SPEED_LOW = 0x0,
+	GPIO_OUTPUT_SPEED_MEDIUM = 0x1,
+	GPIO_OUTPUT_SPEED_HIGH = 0x2,
+	GPIO_OUTPUT_SPEED_VERY_HIGH = 0x3,
+} EGPIO_OutputSpeed;
+
+typedef enum EGPIO_PullUpPullDownMode
+{
+	GPIO_NO_PULL_DONW_AND_UP = 0,
+	GPIO_PULL_DONW = 1,
+	GPIO_PULL_UP = 2,
+} EGPIO_PullUpPullDownMode;
+
+typedef enum EGPIO_PinNumber
+{
+	GPIO_PIN_NUM_0 = 0,
+	GPIO_PIN_NUM_1 = 1,
+	GPIO_PIN_NUM_2 = 2,
+    GPIO_PIN_NUM_3 = 3,
+    GPIO_PIN_NUM_4 = 4,
+    GPIO_PIN_NUM_5 = 5,
+    GPIO_PIN_NUM_6 = 6,
+    GPIO_PIN_NUM_7 = 7,
+    GPIO_PIN_NUM_8 = 8,
+    GPIO_PIN_NUM_9 = 9,
+    GPIO_PIN_NUM_10 = 10,
+    GPIO_PIN_NUM_11 = 11,
+    GPIO_PIN_NUM_12 = 12,
+    GPIO_PIN_NUM_13 = 13,
+    GPIO_PIN_NUM_14 = 14,
+    GPIO_PIN_NUM_15 = 15,
+} EGPIO_PinNumber;
+
+/*
+ * Gpio clock setup
+ */
+void GPIO_PeriClockControl(GPIO_RegDef_t* pGPIOx, EGPIO_ClockControlState clockControlState);
+
+/*
+ * Gpio init/de-init
+ */
+void GPIO_Init(GPIO_Handle_t* pGPIOHandle);
+void GPIO_DeInit(GPIO_RegDef_t* pGPIOx);
+
+/*
+ * Gpio read from input pin/port
+ */
+uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t* pGPIOx, uint8_t pinNumber);
+uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t* pGPIOx);
+
+/*
+ * Gpio write to output pin/port
+ */
+void GPIO_WriteToOutputPin(GPIO_RegDef_t* pGPIOx, uint8_t pinNumber, uint8_t pinState);
+void GPIO_WriteToOutputPort(GPIO_RegDef_t* pGPIOx, uint16_t portState);
+
+/*
+ * Gpio toggle pin output
+ */
+void GPIO_ToggleOutputPin(GPIO_RegDef_t* pGPIOx, uint8_t pinNumber);
+
+/*
+ * Gpio IRQ config
+ */
+void GPIO_IRQConfig(uint8_t IRQNumber, uint8_t IRQPriority, GPIO_IRQControlState cotrolState);
+
+/*
+ * Gpio IRQ callback
+ */
+void GPIO_IRQHandling(uint8_t pinNumber);
+
+
+
+
+
+
+
+
+
 #endif /* INC_STM32407XX_GPIO_DRIVER_H_ */
--- drivers/Src/stm32f407xx_gpio_driver.c
+++ drivers/Src/stm32f407xx_gpio_driver.c
@@ -5,6 +5,301 @@
  *      Author: karol
  */
 
-#include "stm3232f407xx_gpio_driver.h"
+#include "stm32407xx_gpio_driver.h"
+
+/*********************************************************************
+ * @fn      		  - GPIO_PeriClockControl
+ *
+ * @brief             - This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]         - base address of the gpio peripheral
+ * @param[in]         - ENABLE or DISABLE macros
+ * @param[in]         -
+ *
+ * @return            -  none
+ *
+ * @Note              -  none
+
+ */
+void GPIO_PeriClockControl(GPIO_RegDef_t* pGPIOx, EGPIO_ClockControlState clockControlState)
+{
+	if(clockControlState == GPIO_CLOCK_ENABLE)
+	{
+		if(pGPIOx == GPIOA)
+		{
+			GPIOA_PERI_CLOCK_EN();
+		}else if (pGPIOx == GPIOB)
+		{
+			GPIOB_PERI_CLOCK_EN();
+		}else if (pGPIOx == GPIOC)
+		{
+			GPIOC_PERI_CLOCK_EN();
+		}else if (pGPIOx == GPIOD)
+		{
+			GPIOD_PERI_CLOCK_EN();
+		}else if (pGPIOx == GPIOE)
+		{
+			GPIOE_PERI_CLOCK_EN();
+		}else if (pGPIOx == GPIOF)
+		{
+			GPIOF_PERI_CLOCK_EN();
+		}else if (pGPIOx == GPIOG)
+		{
+			GPIOG_PERI_CLOCK_EN();
+		}else if (pGPIOx == GPIOH)
+		{
+			GPIOH_PERI_CLOCK_EN();
+		}else if (pGPIOx == GPIOI)
+		{
+			GPIOI_PERI_CLOCK_EN();
+		}
+		else if (pGPIOx == GPIOJ)
+		{
+			GPIOJ_PERI_CLOCK_EN();
+		}
+		else if (pGPIOx == GPIOK)
+		{
+			GPIOK_PERI_CLOCK_EN();
+		}
+	}
+	else if(clockControlState == GPIO_CLOCK_DISABLE)
+	{
+		if(pGPIOx == GPIOA)
+		{
+			GPIOA_PERI_CLOCK_DIS();
+		}
+		else if (pGPIOx == GPIOB)
+		{
+			GPIOB_PERI_CLOCK_DIS();
+		}
+		else if (pGPIOx == GPIOC)
+		{
+			GPIOC_PERI_CLOCK_DIS();
+		}
+		else if (pGPIOx == GPIOD)
+		{
+			GPIOD_PERI_CLOCK_DIS();
+		}
+		else if (pGPIOx == GPIOE)
+		{
+			GPIOE_PERI_CLOCK_DIS();
+		}
+		else if (pGPIOx == GPIOF)
+		{
+			GPIOF_PERI_CLOCK_DIS();
+		}
+		else if (pGPIOx == GPIOG)
+		{
+			GPIOG_PERI_CLOCK_DIS();
+		}
+		else if (pGPIOx == GPIOH)
+		{
+			GPIOH_PERI_CLOCK_DIS();
+		}
+		else if (pGPIOx == GPIOI)
+		{
+			GPIOI_PERI_CLOCK_DIS();
+		}
+		else if (pGPIOx == GPIOJ)
+		{
+			GPIOJ_PERI_CLOCK_DIS();
+		}
+		else if (pGPIOx == GPIOK)
+		{
+			GPIOK_PERI_CLOCK_DIS();
+		}
+	}
+}
+
+/*********************************************************************
+ * @fn      		  - GPIO_Init
+ *
+ * @brief             - This function
+ *
+ * @param[in]         - base address of the gpio peripheral
+ * @param[in]         -
+ * @param[in]         -
+ *
+ * @return            -  none
+ *
+ * @Note              -  none
+
+ */
+void GPIO_Init(GPIO_Handle_t* pGPIOHandle)
+{
+	uint32_t pinNumber = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber;
+	uint32_t pinMode = pGPIOHandle->GPIO_PinConfig.GPIO_PinMode;
+	uint32_t pinOutputSpeed = pGPIOHandle->GPIO_PinConfig.GPIO_PinOutputSpeed;
+	uint32_t pinOutputType = pGPIOHandle->GPIO_PinConfig.GPIO_PinOutputType;
+	uint32_t pinPullUpPullDownMode = pGPIOHandle->GPIO_PinConfig.GPIO_PinPullUpPullDownMode;
+
+
+	/*Pin mode*/
+	pGPIOHandle->pGPIOx->MODER = (pGPIOHandle->pGPIOx->MODER & ~(pinMode << pinNumber*2)) | (pinMode<< pinNumber*2);
+	/*Pin output speed*/
+	pGPIOHandle->pGPIOx->OSPEEDR = (pGPIOHandle->pGPIOx->OSPEEDR & ~(pinOutputSpeed << pinNumber*2)) | (pinOutputSpeed<< pinNumber*2);
+	/*Pin output type*/
+	pGPIOHandle->pGPIOx->OTYPER = (pGPIOHandle->pGPIOx->OSPEEDR & ~(pinOutputType << pinNumber)) | (pinOutputType<< pinNumber);
+	/*Pin pull up pull down mode*/
+	pGPIOHandle->pGPIOx->PUPDR = (pGPIOHandle->pGPIOx->OSPEEDR & ~(pinPullUpPullDownMode << pinNumber*2)) | (pinPullUpPullDownMode<< pinNumber*2);
+
+
+
+}
+
+/*********************************************************************
+ * @fn      		  - GPIO_PeriClockControl
+ *
+ * @brief             - This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]         - base address of the gpio peripheral
+ * @param[in]         - ENABLE or DISABLE macros
+ * @param[in]         -
+ *
+ * @return            -  none
+ *
+ * @Note              -  none
+
+ */
+void GPIO_DeInit(GPIO_RegDef_t* pGPIOx)
+{
+
+}
+
+/*********************************************************************
+ * @fn      		  - GPIO_PeriClockControl
+ *
+ * @brief             - This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]         - base address of the gpio peripheral
+ * @param[in]         - ENABLE or DISABLE macros
+ * @param[in]         -
+ *
+ * @return            -  none
+ *
+ * @Note              -  none
+
+ */
+uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t* pGPIOx, uint8_t pinNumber)
+{
+
+}
+
+/*********************************************************************
+ * @fn      		  - GPIO_PeriClockControl
+ *
+ * @brief             - This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]         - base address of the gpio peripheral
+ * @param[in]         - ENABLE or DISABLE macros
+ * @param[in]         -
+ *
+ * @return            -  none
+ *
+ * @Note              -  none
+
+ */
+uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t* pGPIOx)
+{
+
+}
+
+/*********************************************************************
+ * @fn      		  - GPIO_PeriClockControl
+ *
+ * @brief             - This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]         - base address of the gpio peripheral
+ * @param[in]         - ENABLE or DISABLE macros
+ * @param[in]         -
+ *
+ * @return            -  none
+ *
+ * @Note              -  none
+
+ */
+void GPIO_WriteToOutputPin(GPIO_RegDef_t* pGPIOx, uint8_t pinNumber, uint8_t pinState)
+{
+
+}
+
+/*********************************************************************
+ * @fn      		  - GPIO_PeriClockControl
+ *
+ * @brief             - This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]         - base address of the gpio peripheral
+ * @param[in]         - ENABLE or DISABLE macros
+ * @param[in]         -
+ *
+ * @return            -  none
+ *
+ * @Note              -  none
+
+ */
+void GPIO_WriteToOutputPort(GPIO_RegDef_t* pGPIOx, uint16_t portState)
+{
+
+}
+
+/*********************************************************************
+ * @fn      		  - GPIO_PeriClockControl
+ *
+ * @brief             - This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]         - base address of the gpio peripheral
+ * @param[in]         - ENABLE or DISABLE macros
+ * @param[in]         -
+ *
+ * @return            -  none
+ *
+ * @Note              -  none
+
+ */
+void GPIO_ToggleOutputPin(GPIO_RegDef_t* pGPIOx, uint8_t pinNumber)
+{
+
+}
+
+/*********************************************************************
+ * @fn      		  - GPIO_PeriClockControl
+ *
+ * @brief             - This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]         - base address of the gpio peripheral
+ * @param[in]         - ENABLE or DISABLE macros
+ * @param[in]         -
+ *
+ * @return            -  none
+ *
+ * @Note              -  none
+
+ */
+void GPIO_IRQConfig(uint8_t IRQNumber, uint8_t IRQPriority, GPIO_IRQControlState cotrolState)
+{
+
+}
+
+/*********************************************************************
+ * @fn      		  - GPIO_IRQHandling
+ *
+ * @brief             - This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]         - base address of the gpio peripheral
+ * @param[in]         - ENABLE or DISABLE macros
+ * @param[in]         -
+ *
+ * @return            -  none
+ *
+ * @Note              -  none
+
+ */
+void GPIO_IRQHandling(uint8_t pinNumber)
+{
+
+}
+
+
+
 
 
