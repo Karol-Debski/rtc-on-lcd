8efe7140e2be202cfda8a2d18b0bb493a7e17b01
Karol <karol3129@gmail.com>
2024-10-27 08:45:22 +0100

    Added main file and deleted demos for private hal drivers

---
+++ Src/main.c
@@ -0,0 +1,25 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 27, 2024
+ *      Author: karol
+ */
+
+
+#include <stdint.h>
+#include <string.h>
+
+//#if !defined(__SOFT_FP__) && defined(__ARM_FP)
+//  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+//#endif
+
+#include "stm32f407xx.h"
+
+
+
+int main(void)
+{
+
+	while(1);
+}
+
